de6f24977266c4635a8d3740c58a49ec8412865c
jige06 <jghebert06@gmail.com>
2026-05-11 12:13:11 +0200

    views: ajout header et footer + style.css

---
+++ src/views/layouts/footer.php
@@ -0,0 +1,25 @@
+        <footer>
+            <div class="container-fluid text-center">
+                <div class="row">
+                    <div class="col">
+                        <p>Du lundi au jeudi : 9h00 - 19h00</p>
+                        <p>Du vendredi au dimanche : 9h00 - 17h00</p>
+                    </div>
+                    <div class="col">
+                        <p>Vite & Gourmand</p>
+                        <p>19 rue Bouffard</p>
+                        <p>33000 Bordeaux</p>
+                        <p>05 57 84 12 34</p>
+                    </div>
+                    <div class="lien col">
+                        <a href="mailto:contact@viteetgourmand.com">contact@viteetgourmand.com</a>
+                    </div>
+                </div>
+                <div class="lien text-center">
+                    <a href="#">Mentions légales</a>
+                    <a href="#">Conditions générales de ventes</a>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ src/views/layouts/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
+    <link rel="stylesheet" href="../../../public/assets/css/style.css">
+    <title>Vite & Gourmand</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container">
+                <a class="navbar-brand" href="/">Vite & Gourmand</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="/">Accueil</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/menus">Nos menus</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/connexion">Connexion</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/inscription">S'inscrire</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
