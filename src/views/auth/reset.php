e35f96cc9f886217f1a02e30039db157cd39de6b
jige06 <jghebert06@gmail.com>
2026-05-11 16:18:06 +0200

    views: ajout reset.php

---
+++ src/views/auth/reset.php
@@ -0,0 +1,29 @@
+<?php require_once('../src/views/layouts/header.php'); ?>
+
+<div class="big-title container-fluid">
+    <img src="/public/assets/images/Buffet_Big_title.png" alt="buffet">
+    <h2 class="text-center">Changer mon mot de passe</h2>
+</div>
+<div class="box-connexion container-fluid">
+    <?php if (isset($_SESSION['error'])): ?>
+        <div class="alert alert-danger"><?= $_SESSION['error'];
+                                        unset($_SESSION['error']); ?></div>
+    <?php endif; ?>
+    <?php if (isset($_SESSION['message'])): ?>
+        <div class="alert alert-info"><?= $_SESSION['message'];
+                                        unset($_SESSION['message']); ?></div>
+    <?php endif; ?>
+    <form class="d-flex flex-column align-items-center" action="/mot-de-passe-oublie" method="post">
+        <div class="connexion text-start mb-4">
+            <label class="mb-1 d-block" for="email">Email</label>
+            <input class="form-control" type="email" id="email" name="email" required placeholder="exemple@exemple.com">
+        </div>
+
+        <button class="connect-button mb-3 mt-3" type="submit">Recevoir mon mot de passe temporaire</button>
+        <div class=" mt-1">
+            <p>Vous vous rappelez de votre mot de passe ! <a href="/connexion">Se connecter</a></p>
+        </div>
+    </form>
+</div>
+
+<?php require_once('../src/views/layouts/footer.php'); ?>
