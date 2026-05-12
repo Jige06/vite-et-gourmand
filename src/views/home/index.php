8f7eae319f33832b984706acc88e25aaf86c2abe
jige06 <jghebert06@gmail.com>
2026-05-12 02:11:17 +0200

    views: ajout page accueil + style.css mis à jour

---
+++ src/views/home/index.php
@@ -0,0 +1,85 @@
+<?php require_once('../src/views/layouts/header.php'); ?>
+
+<div class="big-title-accueil container-fluid">
+    <img src="/public/assets/images/Buffet_Big_title.png" alt="buffet">
+    <h3 class="text-center">Julie et José vous souhaite la bienvenue chez</h3>
+    <h2 class="text-center">Vite & Gourmand</h2>
+    <button onclick="window.location.href='/menus'" class="big-title-button mb-3 mt-3" type="submit">Découvrir nos menus</button>
+</div>
+
+<div class="presentation container-fluid text-center mb-4 mt-4 pt-4 pb-4">
+    <div class="titre-presentation pb-4 pt-4">
+        <h3>Notre entreprise</h3>
+    </div>
+    <div class="text-presentation pb-1">
+        <p>Portée par Julie et José, notre équipe met son professionnalisme au service de vos événements. Des repas de fêtes (Noël, Pâques) aux grandes célébrations (mariages, séminaires), nous proposons des menus savoureux en constante évolution, adaptés à tous les régimes. Profitez de notre savoir-faire artisanal et d'une commande simplifiée pour faire de vos réceptions un moment gastronomique d’exception.</p>
+    </div>
+    <div class="image-presentation">
+        <img src="/public/assets/images/Julie_et_José.png" alt="Julie_et_José_en_photo">
+    </div>
+</div>
+
+<div class="realisations container-fluid text-center mb-4 mt-4 pb-4 pt-4">
+    <div class="titre-realisation pb-4 pt-4">
+        <h3>Nos réalisations</h3>
+    </div>
+    <div class="text-realisation pb-1">
+        <p>Découvrez nos dernières créations culinaires et laissez-vous inspirer pour votre prochain événement.</p>
+    </div>
+    <div class="row image mt-4">
+        <div class="col-12 col-md-12 col-lg-6 image-realisation mb-4">
+            <img src="/public/assets/images/asperges_sauce_mousseline.png" alt="asperges_sauce_mousseline">
+        </div>
+        <div class="col-12 col-md-12 col-lg-6 image-realisation mb-4">
+            <img src="/public/assets/images/buffet_entrees_maraicheres.png" alt="buffet_entrees_maraicheres">
+        </div>
+    </div>
+    <div class="row image">
+        <div class="col-12 col-md-12 col-lg-6 image-realisation mb-4">
+            <img src="/public/assets/images/cochon_lait_roti_broche.png" alt="cochon_lait_roti_broche">
+        </div>
+        <div class="col-12 col-md-12 col-lg-6 image-realisation mb-4">
+            <img src="/public/assets/images/filet_boeuf_en_croute_juscorse.png" alt="filet_boeuf_en_croute_juscorse">
+        </div>
+    </div>
+    <div class="row image">
+        <div class="col-12 col-md-12 col-lg-6 image-realisation mb-4">
+            <img src="/public/assets/images/plateaux_fromages_france.png" alt="plateaux_fromages_france">
+        </div>
+        <div class="col-12 col-md-12 col-lg-6 image-realisation mb-4">
+            <img src="/public/assets/images/trio_gourmandises_chocolatees.png" alt="trio_gourmandises_chocolatees">
+        </div>
+    </div>
+    <button onclick="window.location.href='/menus'" class="connect-button mb-3 mt-3" type="submit">Découvrir nos menus</button>
+</div>
+
+<div class="section-avis container-fluid text-start mb-4 mt-4 pb-4 pt-4">
+    <div class="titre-avis text-center pb-4 pt-4">
+        <h3>Quelques avis clients</h3>
+    </div>
+    <div class="avis ps-3 pe-3 pt-3 mt-2 mb-2">
+        <p>Note: 4/5 * * * *</p>
+        <p>Prenom: Marc-Antoine</p>
+        <p>"Un service d'une qualité rare à Bordeaux. Nous avons fait appel à Julie et José pour notre mariage et tout était parfait, du vin d'honneur au dessert. Le filet de bœuf en croûte a fait l'unanimité auprès de nos invités ! Professionnalisme et discrétion assurés."</p>
+    </div>
+    <div class="avis ps-3 pe-3 pt-3 mt-2 mb-2">
+        <p>Note: 4/5 * * * *</p>
+        <p>Prenom: Marc-Antoine</p>
+        <p>"Un service d'une qualité rare à Bordeaux. Nous avons fait appel à Julie et José pour notre mariage et tout était parfait, du vin d'honneur au dessert. Le filet de bœuf en croûte a fait l'unanimité auprès de nos invités ! Professionnalisme et discrétion assurés."</p>
+    </div>
+    <div class="avis ps-3 pe-3 pt-3 mt-2 mb-2">
+        <p>Note: 4/5 * * * *</p>
+        <p>Prenom: Marc-Antoine</p>
+        <p>"Un service d'une qualité rare à Bordeaux. Nous avons fait appel à Julie et José pour notre mariage et tout était parfait, du vin d'honneur au dessert. Le filet de bœuf en croûte a fait l'unanimité auprès de nos invités ! Professionnalisme et discrétion assurés."</p>
+    </div>
+    <div class="avis ps-3 pe-3 pt-3 mt-2 mb-2">
+        <p>Note: 4/5 * * * *</p>
+        <p>Prenom: Marc-Antoine</p>
+        <p>"Un service d'une qualité rare à Bordeaux. Nous avons fait appel à Julie et José pour notre mariage et tout était parfait, du vin d'honneur au dessert. Le filet de bœuf en croûte a fait l'unanimité auprès de nos invités ! Professionnalisme et discrétion assurés."</p>
+    </div>
+    <div class="avis-button text-center">
+        <button onclick="window.location.href='/avis'" class="connect-button mb-3 mt-3" type="submit">Lire les avis</button>
+    </div>
+</div>
+
+<?php require_once('../src/views/layouts/footer.php'); ?>
