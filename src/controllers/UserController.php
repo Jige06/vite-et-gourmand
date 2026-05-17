bb9339f2fd0f133077f6cbe999d53095974a15e2
jige06 <jghebert06@gmail.com>
2026-05-17 19:34:08 +0200

    docs: documentation du code UserController

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -2,13 +2,16 @@
 
 class UserController
 {
+    // Methode qui appelle le model pour mettre a jour le profil de l'utilisateur
     public function updateProfil()
     {
+        // Vérification si un utilisateur est connecté
         if (!isset($_SESSION['id_user'])) {
             Auth::redirect('/connexion');
             return;
         }
 
+        // Récupération des données de l'utilisateur saisies dans le formulaire de modification
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $nom = trim(htmlspecialchars($_POST['nom']));
             $prenom = trim(htmlspecialchars($_POST['prenom']));
@@ -19,9 +22,17 @@ class UserController
             $ville = trim(htmlspecialchars($_POST['ville']));
             $idUser = $_SESSION['id_user'];
 
-            UserModel::updateProfil($idUser, $nom, $prenom, $email, $telephone, $adresse, $codePostal, $ville);
+            // Appel de la methode de mise à jour en BDD
+            $result = UserModel::updateProfil($idUser, $nom, $prenom, $email, $telephone, $adresse, $codePostal, $ville);
 
-            // Mise à jour de la session
+            // Si nouveau email saisi deja saisi en bdd --> message de refus de mis a jour
+            if ($result === false) {
+                $_SESSION['error'] = "Cet email n'est pas disponible, veuillez en choisir un autre.";
+                Auth::redirect('/mon-espace/commandes');
+                return;
+            }
+
+            // Mise à jour de la session avec message de succès
             $_SESSION['nom'] = $nom;
             $_SESSION['prenom'] = $prenom;
             $_SESSION['email'] = $email;
