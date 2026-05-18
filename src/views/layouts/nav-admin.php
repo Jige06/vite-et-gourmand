3b8079292acae46e06231edaebc74fe7f155ef80
jige06 <jghebert06@gmail.com>
2026-05-18 19:53:26 +0200

    ajout nav espace admin

---
+++ src/views/layouts/nav-admin.php
@@ -0,0 +1,8 @@
+<div class="container mt-3 mb-3 text-center">
+    <div class="d-flex gap-3 justify-content-center">
+        <a href="/employe" class="connect-button">Commandes</a>
+        <a href="/employe/menus" class="connect-button">Menus</a>
+        <a href="/employe/avis" class="connect-button">Avis</a>
+        <a href="/admin/stats" class="connect-button">Statistiques</a>
+    </div>
+</div>
