42b802664b1be87178c5a4f83632054c18885e00
jige06 <jghebert06@gmail.com>
2026-05-18 20:29:47 +0200

    ajout nav-employe.php layout

---
+++ src/views/layouts/nav-employe.php
@@ -0,0 +1,7 @@
+<div class="container mt-3 mb-3 text-center">
+    <div class="d-flex gap-3 justify-content-center">
+        <a href="/employe" class="connect-button">Commandes</a>
+        <a href="/employe/menus" class="connect-button">Menus</a>
+        <a href="/employe/avis" class="connect-button">Avis</a>
+    </div>
+</div>
