d41cf57705a8f3a5cd583a42e13d4dfac5956081
jige06 <jghebert06@gmail.com>
2026-05-20 00:08:55 +0200

    ContactController - formulaire contact

---
+++ src/controllers/ContactController.php
@@ -0,0 +1,32 @@
+<?php
+
+class ContactController
+{
+
+    // Méthode qui gère le formulaire de contact
+    public function handleContact()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $titre = trim(htmlspecialchars($_POST['titre']));
+            $description = trim(htmlspecialchars($_POST['description']));
+            $email = trim(htmlspecialchars($_POST['email']));
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $_SESSION['error'] = "L'adresse email n'est pas valide.";
+                Auth::redirect('/contact');
+            }
+
+            ContactModel::sendContactMail($titre, $description, $email);
+            $_SESSION['success'] = "Votre message a bien été envoyé !";
+            Auth::redirect('/contact');
+        } else {
+            $this->showContact();
+        }
+    }
+
+    //Méthode qui affiche la vue de contact
+    public function showContact()
+    {
+        require_once(__DIR__ . '/../views/contact/index.php');
+    }
+}
